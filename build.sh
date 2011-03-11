07789f5af4a90c73a5f13ed5cb54a371ae380dea
Victor Engmark <victor.engmark@gmail.com>
2011-03-11 18:49:25 +0100

    Makefile instead of build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Requires rpm package
-# Reintroduce bdist_wininst when it stops failing on Linux
-cd $(dirname $0) && \
-python setup.py test && \
-python setup.py $1 bdist_egg bdist_rpm sdist upload clean && \
-rm -fr *.pyc build dist *.egg-info
